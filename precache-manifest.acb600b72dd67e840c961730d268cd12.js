34f0a9cb4380d1d0eb694a6a10a735191964132b
Jaeheon Shim <git@jaeheonshim.com>
2020-09-02 18:05:58 -0400

    Updates

---
+++ precache-manifest.acb600b72dd67e840c961730d268cd12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232013b9e6492408118c2f8159103282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdf8bf551a2b0a400c7f",
+    "url": "/static/js/2.8fcc3be4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8fcc3be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d86db0e844451b392f8",
+    "url": "/static/js/main.e1eb4f57.chunk.js"
+  },
+  {
+    "revision": "38b1c0716d29e8ce568c",
+    "url": "/static/js/runtime-main.4100cdbb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/schoolschedule/precache-manifest.a0260a3687d61abd91ac8978a47cfacf.js"
+  "/precache-manifest.acb600b72dd67e840c961730d268cd12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/schoolschedule/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
